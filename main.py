880c7f21ec3d1c46605521375d876410118f1639
mosi202020 <148450390+mosi202020@users.noreply.github.com>
2025-07-07 21:12:27 +0330

    Update main.py

--- main.py
+++ main.py
@@ -32,5 +32,5 @@ def send_signal():
         return f"❌ Failed: {r.text}", 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
